ba1632e902d14a91bf50674373d5613ba75a4d28
gnzlbg <gonzalobg88@gmail.com>
2018-01-15 17:13:12 +0100

    set benchmark to debug

--- ci/run.sh
+++ ci/run.sh
@@ -5,7 +5,7 @@ set -ex
 export RUST_TEST_THREADS=1
 export RUST_BACKTRACE=1
 
-cargo test --target=$TARGET
-cargo test --target=$TARGET --release
-cargo bench
+cargo test --target=$TARGET -- --nocapture
+cargo test --target=$TARGET --release -- --nocapture
+cargo bench -- --nocapture
 cargo doc
