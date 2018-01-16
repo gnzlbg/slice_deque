b66ac25d1763bf7791591e49be78ba231f042aed
gnzlbg <gonzalobg88@gmail.com>
2018-01-16 18:53:30 +0100

    Support all POSIX-systems via SysV shared memory.
    This commit adds a racy memory-mirroring algorithm
    on top of System V shared memory, and implements
    a non-racy Linux-specific algorithm for linux
    systems (improving over the previous racy one).

---
+++ src/mirrored/linux.rs
@@ -0,0 +1,125 @@
+//! Non-racy linux-specific mirrored memory allocation.
+
+use libc::{c_char, c_void, close, ftruncate, mkstemp, mmap, munmap, size_t,
+           sysconf, MAP_FAILED, MAP_FIXED, MAP_SHARED, PROT_READ, PROT_WRITE,
+           _SC_PAGESIZE};
+
+/// Returns the size of a memory allocation unit.
+///
+/// In Linux-like systems this equals the page-size.
+pub fn allocation_granularity() -> usize {
+    unsafe { sysconf(_SC_PAGESIZE) as usize }
+}
+
+/// Allocates an uninitialzied buffer that holds `size` bytes, where
+/// the bytes in range `[0, size / 2)` are mirrored into the bytes in
+/// range `[size / 2, size)`.
+///
+/// On Linux the algorithm is as follows:
+///
+/// * 1. Allocate a memory-mapped file containing `size / 2` bytes.
+/// * 2. Map the file into `size` bytes of virtual memory.
+/// * 3. Map the file into the last `size / 2` bytes of the virtual memory
+/// region      obtained in step 2.
+///
+/// This algorithm doesn't have any races.
+///
+/// # Panics
+///
+/// If `size` is zero or `size / 2` is not a multiple of the
+/// allocation granularity.
+pub fn allocate_mirrored(size: usize) -> Result<*mut u8, ()> {
+    unsafe {
+        let half_size = size / 2;
+        assert!(size != 0);
+        assert!(half_size % allocation_granularity() == 0);
+
+        // create temporary file
+        let mut fname = *b"/tmp/slice_deque_fileXXXXXX\0";
+        let fd = mkstemp(fname.as_mut_ptr() as *mut c_char);
+        if fd == -1 {
+            print_error("mkstemp failed");
+            return Err(());
+        }
+        if ftruncate(fd, half_size as i64) == -1 {
+            print_error("ftruncate failed");
+            if close(fd) == -1 {
+                print_error("@ftruncate: close failed");
+            }
+            return Err(());
+        };
+
+        // mmap memory
+        let ptr = mmap(
+            0 as *mut c_void,
+            size,
+            PROT_READ | PROT_WRITE,
+            MAP_SHARED,
+            fd,
+            0,
+        );
+        if ptr == MAP_FAILED {
+            print_error("@first: mmap failed");
+            if close(fd) == -1 {
+                print_error("@first: close failed");
+            }
+            return Err(());
+        }
+
+        let ptr2 = mmap(
+            (ptr as *mut u8).offset(half_size as isize) as *mut c_void,
+            half_size,
+            PROT_READ | PROT_WRITE,
+            MAP_SHARED | MAP_FIXED,
+            fd,
+            0,
+        );
+        if ptr2 == MAP_FAILED {
+            print_error("@second: mmap failed");
+            if munmap(ptr, size as size_t) == -1 {
+                print_error("@second: munmap failed");
+            }
+            if close(fd) == -1 {
+                print_error("@second: close failed");
+            }
+            return Err(());
+        }
+
+        if close(fd) == -1 {
+            print_error("@success: close failed");
+        }
+        Ok(ptr as *mut u8)
+    }
+}
+
+/// Deallocates the mirrored memory region at `ptr` of `size` bytes.
+///
+/// # Unsafe
+///
+/// `ptr` must have been obtained from a call to `allocate_mirrored(size)`,
+/// otherwise the behavior is undefined.
+///
+/// # Panics
+///
+/// If `size` is zero or `size / 2` is not a multiple of the
+/// allocation granularity, or `ptr` is null.
+pub unsafe fn deallocate_mirrored(ptr: *mut u8, size: usize) {
+    assert!(!ptr.is_null());
+    assert!(size != 0);
+    assert!(size % allocation_granularity() == 0);
+    if munmap(ptr as *mut c_void, size as size_t) == -1 {
+        print_error("deallocate munmap failed");
+    }
+}
+
+#[cfg(debug_assertions)]
+fn print_error(location: &str) {
+    eprintln!(
+        "Error at {}: {}",
+        location,
+        ::std::io::Error::last_os_error()
+    );
+}
+
+#[cfg(not(debug_assertions))]
+fn print_error(_location: &str) {}
---
+++ src/mirrored/sysv.rs
@@ -0,0 +1,224 @@
+//! Racy System V mirrored memory allocation.
+use libc::{c_int, c_void, mmap, munmap, shmat, shmctl, shmdt, shmget,
+           shmid_ds, sysconf, IPC_CREAT, IPC_PRIVATE, IPC_RMID,
+           MAP_ANONYMOUS, MAP_FAILED, MAP_PRIVATE, PROT_NONE, _SC_PAGESIZE};
+
+/// Returns the size of an allocation unit.
+///
+/// System V shared memory has the page size as its allocation unit.
+pub fn allocation_granularity() -> usize {
+    unsafe { sysconf(_SC_PAGESIZE) as usize }
+}
+
+/// System V Shared Memory handle.
+struct SharedMemory {
+    id: c_int,
+}
+
+/// Map of System V Shared Memory to an address in the process address space.
+struct MemoryMap(*mut c_void);
+
+impl SharedMemory {
+    /// Allocates `size` bytes of inter-process shared memory.
+    ///
+    /// Return the handle to this shared memory.
+    ///
+    /// # Panics
+    ///
+    /// If `size` is zero or not a multiple of the allocation granularity.
+    pub fn allocate(size: usize) -> Result<SharedMemory, ()> {
+        assert!(size != 0);
+        assert!(size % allocation_granularity() == 0);
+        unsafe {
+            let id = shmget(IPC_PRIVATE, size, IPC_CREAT | 448);
+            if id == -1 {
+                print_error("shmget");
+                return Err(());
+            }
+            Ok(SharedMemory { id })
+        }
+    }
+
+    /// Attaches System V shared memory to the memory address at `ptr` in the
+    /// address space of the current process.
+    ///
+    /// # Panics
+    ///
+    /// If `ptr` is null.
+    pub fn attach(&self, ptr: *mut c_void) -> Result<MemoryMap, ()> {
+        unsafe {
+            // note: the success of allocate guarantees `shm_id != -1`.
+            assert!(!ptr.is_null());
+            let r = shmat(self.id, ptr, 0);
+            if r as isize == -1 {
+                print_error("shmat");
+                return Err(());
+            }
+            let map = MemoryMap(ptr);
+            if r != ptr {
+                print_error("shmat2");
+                // map is dropped here, freeing the memory.
+                return Err(());
+            }
+            Ok(map)
+        }
+    }
+}
+
+impl Drop for SharedMemory {
+    /// Deallocates the inter-process shared memory..
+    fn drop(&mut self) {
+        unsafe {
+            // note: the success of allocate guarantees `shm_id != -1`.
+            let r = shmctl(self.id, IPC_RMID, 0 as *mut shmid_ds);
+            if r == -1 {
+                // TODO: unlikely
+                // This should never happen, but just in case:
+                print_error("shmctl");
+                panic!("freeing system V shared-memory failed");
+            }
+        }
+    }
+}
+
+impl MemoryMap {
+    /// Initializes a MemoryMap to `ptr`.
+    ///
+    /// # Panics
+    ///
+    /// If `ptr` is null.
+    ///
+    /// # Unsafety
+    ///
+    /// If `ptr` does not point to a memory map created using
+    /// `SharedMemory::attach` that has not been dropped yet..
+    unsafe fn from_raw(ptr: *mut c_void) -> Result<MemoryMap, ()> {
+        assert!(!ptr.is_null());
+        Ok(MemoryMap(ptr))
+    }
+}
+
+impl Drop for MemoryMap {
+    fn drop(&mut self) {
+        unsafe {
+            // note: the success of SharedMemory::attach and MemoryMap::new
+            // guarantee `!self.0.is_null()`.
+            let r = shmdt(self.0);
+            if r == -1 {
+                // TODO: unlikely
+                print_error("shmdt");
+                panic!("freeing system V memory map failed");
+            }
+        }
+    }
+}
+
+/// Allocates `size` bytes of uninitialized memory, where the bytes in range
+/// `[0, size / 2)` are mirrored into the bytes in range `[size / 2, size)`.
+///
+/// The algorithm using System V interprocess shared-memory is:
+///
+/// * 1. Allocate `size / 2` of interprocess shared memory.
+/// * 2. Reserve `size` bytes of virtual memory using `mmap + munmap`.
+/// * 3. Attach the shared memory to the first and second half.
+///
+/// There is a race between steps 2 and 3 because after unmapping the memory
+/// and before attaching the shared memory to it another process might use that
+/// memory.
+pub fn allocate_mirrored(size: usize) -> Result<*mut u8, ()> {
+    unsafe {
+        assert!(size != 0);
+        let half_size = size / 2;
+        assert!(half_size % allocation_granularity() == 0);
+
+        // 1. Allocate interprocess shared memory
+        let shm = SharedMemory::allocate(half_size)?;
+
+        const MAX_NO_ITERS: i32 = 5;
+        let mut counter = 0;
+        let ptr = loop {
+            counter += 1;
+            if counter > MAX_NO_ITERS {
+                return Err(());
+            }
+
+            // 2. Reserve virtual memory:
+            let ptr = mmap(
+                0 as *mut c_void,
+                size,
+                PROT_NONE,
+                MAP_ANONYMOUS | MAP_PRIVATE,
+                -1,
+                0,
+            );
+            if ptr == MAP_FAILED {
+                print_error("mmap initial");
+                return Err(());
+            }
+
+            let ptr2 =
+                (ptr as *mut u8).offset(half_size as isize) as *mut c_void;
+
+            unmap(ptr, size).expect("unmap initial failed");
+
+            // 3. Attach shared memory to virtual memory:
+            let map0 = shm.attach(ptr);
+            if map0.is_err() {
+                print_error("attach_shm first failed");
+                continue;
+            }
+            let map1 = shm.attach(ptr2);
+            if map1.is_err() {
+                print_error("attach_shm second failed");
+                continue;
+            }
+            // On success we leak the maps to keep them alive
+            ::std::mem::forget(map0);
+            ::std::mem::forget(map1);
+            break ptr;
+        };
+
+        Ok(ptr as *mut u8)
+    }
+}
+
+/// Deallocates the mirrored memory region at `ptr` of `size` bytes.
+///
+/// # Unsafe
+///
+/// `ptr` must have been obtained from a call to `allocate_mirrored(size)` and
+/// not have been previously deallocated. Otherwise the behavior is undefined.
+///
+/// # Panics
+///
+/// If `size` is zero or `size / 2` is not a multiple of the
+/// allocation granularity, or `ptr` is null.
+pub unsafe fn deallocate_mirrored(ptr: *mut u8, size: usize) {
+    let ptr2 = ptr.offset(size as isize / 2);
+    MemoryMap::from_raw(ptr as *mut c_void)
+        .expect("deallocate mirrored first failed");
+    MemoryMap::from_raw(ptr2 as *mut c_void)
+        .expect("deallocate mirrored second failed");
+}
+
+/// Unmaps the memory region at `[ptr, ptr+size)`.
+unsafe fn unmap(ptr: *mut c_void, size: usize) -> Result<(), ()> {
+    let r = munmap(ptr, size);
+    if r == -1 {
+        print_error("unmap");
+        return Err(());
+    }
+    Ok(())
+}
+
+#[cfg(not(debug_assertions))]
+fn print_error(_location: &str) {}
+
+#[cfg(debug_assertions)]
+fn print_error(location: &str) {
+    eprintln!(
+        "Error at {}: {}",
+        location,
+        ::std::io::Error::last_os_error()
+    );
+}
