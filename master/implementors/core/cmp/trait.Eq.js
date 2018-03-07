d0554d05ae686e6aaed2c033fc4c6373fcc27b14
Travis CI User <travis@example.org>
2018-03-07 18:03:42 +0000

    Automatic Travis documentation build

--- master/implementors/core/cmp/trait.Eq.js
+++ master/implementors/core/cmp/trait.Eq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["slice_deque"] = ["impl&lt;T:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"slice_deque/struct.SliceDeque.html\" title=\"struct slice_deque::SliceDeque\">SliceDeque</a>&lt;T&gt;",];
+implementors["slice_deque"] = [{text:"impl&lt;T:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"slice_deque/struct.SliceDeque.html\" title=\"struct slice_deque::SliceDeque\">SliceDeque</a>&lt;T&gt;",synthetic:false,types:["slice_deque::SliceDeque"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- master/implementors/core/ops/deref/trait.Deref.js
+++ master/implementors/core/ops/deref/trait.Deref.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["slice_deque"] = ["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"slice_deque/struct.SliceDeque.html\" title=\"struct slice_deque::SliceDeque\">SliceDeque</a>&lt;T&gt;",];
+implementors["slice_deque"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"slice_deque/struct.SliceDeque.html\" title=\"struct slice_deque::SliceDeque\">SliceDeque</a>&lt;T&gt;",synthetic:false,types:["slice_deque::SliceDeque"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
