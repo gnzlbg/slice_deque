7ce04b3457b4989fb984ed638eae99d86ec3d092
Travis CI User <travis@example.org>
2018-12-05 12:27:14 +0000

    Automatic Travis documentation build

---
+++ master/source-files.js
@@ -0,0 +1,2 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex['slice_deque'] = {"name":"","dirs":[{"name":"mirrored","dirs":[],"files":["buffer.rs","linux.rs","mod.rs"]}],"files":["lib.rs","macros.rs"]};
