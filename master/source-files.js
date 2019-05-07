3b4c36e79fa79c39c9ae56d1d904d81454c218a4
Travis CI User <travis@example.org>
2019-05-07 13:37:33 +0000

    Automatic Travis documentation build

--- master/source-files.js
+++ master/source-files.js
@@ -1,2 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["slice_deque"] = {"name":"","dirs":[{"name":"mirrored","dirs":[],"files":["buffer.rs","linux.rs","mod.rs"]}],"files":["lib.rs","macros.rs"]};
+sourcesIndex["slice_deque"] = {"name":"","dirs":[{"name":"mirrored","files":["buffer.rs","linux.rs","mod.rs"]}],"files":["lib.rs","macros.rs"]};
+createSourceSidebar();
