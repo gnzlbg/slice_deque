60d8a3222a1ac7a8177558d9e4c5a2cb90948025
gnzlbg <gonzalobg88@gmail.com>
2019-05-07 13:21:48 +0200

    Use panic=abort only for the memory sanitizer

--- ci/run_san.sh
+++ ci/run_san.sh
@@ -6,11 +6,15 @@ export RUST_TEST_THREADS=1
 export RUST_BACKTRACE=1
 export RUST_TEST_NOCAPTURE=1
 
-export RUSTFLAGS="-C panic=abort -Z sanitizer=${SANITIZER}"
+export RUSTFLAGS="${RUSTFLAGS} -Z sanitizer=${SANITIZER}"
 export ASAN_OPTIONS="detect_odr_violation=0:detect_leaks=0"
 
 export OPT="--no-default-features --features=unstable --target=x86_64-unknown-linux-gnu"
 export OPT_RELEASE="--release ${OPT}"
+
+if [[ $SANITIZER == "memory" ]]; then
+    export RUSTFLAGS="${RUSTFLAGS} -C panic=abort"
+fi
 
 if [[ $SANITIZER == "address" ]]; then
     cargo test --lib $OPT
